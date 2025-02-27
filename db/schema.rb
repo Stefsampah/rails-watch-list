71e802c7cfea9ad0eab9244e8ef40c7d94738d1e
stephane sampah gh api stefsampah@hotmail.com <stefsampah@hotmail.com>
2025-02-27 11:41:03 +0100

    add bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_27_103648) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_27_104018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.integer "movie_id"
+    t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
